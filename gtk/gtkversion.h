8e840f126196031fdb0bf94e5439dfbc3a8fb4c1
Bill Kelly <billk@camerabits.com>
2015-04-16 03:23:12 -0700

    Updated generated files from 3.14.7 to 3.16.2

--- gtk/gtkversion.h
+++ gtk/gtkversion.h
@@ -55,7 +55,7 @@
  * application compile time, rather than from the library linked
  * against at application run time.
  */
-#define GTK_MINOR_VERSION (14)
+#define GTK_MINOR_VERSION (16)
 
 /**
  * GTK_MICRO_VERSION:
@@ -64,7 +64,7 @@
  * application compile time, rather than from the library linked
  * against at application run time.
  */
-#define GTK_MICRO_VERSION (7)
+#define GTK_MICRO_VERSION (2)
 
 /**
  * GTK_BINARY_AGE:
